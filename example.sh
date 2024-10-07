09261ade35a6f763169c83c8e99eb63b7d3bee72
Jan Kamiński <mr.johny.kaminski@gmail.com>
2024-10-07 08:59:33 +0200

    Add progress_bar_color example in example.sh bash script

--- example.sh
+++ example.sh
@@ -5,7 +5,7 @@ source progress-bar.sh
 # Example usage
 for i in {1..50}; do
     fill_to_half_with_space "Progress value: $i"
-    progress_bar "$i" 50
+    progress_bar_color "$i" 50 "92m"
     sleep 0.1
 done
 
